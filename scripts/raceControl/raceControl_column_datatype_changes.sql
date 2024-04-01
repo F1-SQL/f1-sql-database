46a80d708980e82441d81470b6c976085d3f6b70
Rich Howell <github@codenameowl.com>
2024-04-01 18:36:57 +0100

    Update raceControl_column_datatype_changes.sql
    Changed column name

--- scripts/raceControl/raceControl_column_datatype_changes.sql
+++ scripts/raceControl/raceControl_column_datatype_changes.sql
@@ -2,7 +2,7 @@ ALTER TABLE [SequelFormulaDevTest].[dbo].[raceControl] ALTER COLUMN meeting_key 
 ALTER TABLE [SequelFormulaDevTest].[dbo].[raceControl] ALTER COLUMN session_key INT;
 ALTER TABLE [SequelFormulaDevTest].[dbo].[raceControl] ALTER COLUMN sector INT;
 ALTER TABLE [SequelFormulaDevTest].[dbo].[raceControl] ALTER COLUMN [lap_number] INT;
-ALTER TABLE [SequelFormulaDevTest].[dbo].[raceControl] ALTER COLUMN [driver_number] INT;
+ALTER TABLE [SequelFormulaDevTest].[dbo].[raceControl] ALTER COLUMN [driver_key] INT;
 ALTER TABLE [SequelFormulaDevTest].[dbo].[raceControl] ALTER COLUMN [flag] VARCHAR(20);
 ALTER TABLE [SequelFormulaDevTest].[dbo].[raceControl] ALTER COLUMN [scope] VARCHAR(10);
 ALTER TABLE [SequelFormulaDevTest].[dbo].[raceControl] ALTER COLUMN [category] VARCHAR(10);
