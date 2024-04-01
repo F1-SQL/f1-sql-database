5214c362d0a363a1000d26a88faf46c4016140ec
Rich Howell <github@codenameowl.com>
2024-04-01 15:02:49 +0100

    New Scripts
    Added new scripts to handle additional files

--- scripts/drivers/drivers_delete_duplicate_records.sql
+++ scripts/drivers/drivers_delete_duplicate_records.sql
@@ -1,34 +1,11 @@
-;WITH DuplicateDrivers AS ( 
+ ;WITH DuplicateDrivers AS ( 
 
-SELECT 
-    driver_key, 
-    meeting_key, 
-    session_key,
-    ROW_NUMBER() OVER(PARTITION BY driver_key, meeting_key, session_key ORDER BY driver_key) as RowNumber  
-FROM 
-    [dbo].[drivers] 
-)
+ SELECT 
+     driver_key, 
+	 team_key,
+     ROW_NUMBER() OVER(PARTITION BY driver_key ORDER BY driver_key) as RowNumber  
+ FROM 
+     [dbo].[drivers] 
+ )
 
-DELETE FROM DuplicateDrivers WHERE RowNumber > 1
-
-GO
-
-;WITH Drivers AS
-(
-SELECT [broadcast_name]
-      ,[country_code]
-      ,[driver_key]
-      ,[first_name]
-      ,[full_name]
-      ,[headshot_url]
-      ,[last_name]
-      ,[meeting_key]
-      ,[name_acronym]
-      ,[session_key]
-      ,[team_colour]
-      ,[team_key]
-	  ,ROW_NUMBER() OVER(PARTITION BY driver_key ORDER BY driver_key) AS RowNumber
-  FROM [dbo].[drivers]
-)
-
-DELETE FROM Drivers WHERE RowNumber > 1;
+DELETE FROM DuplicateDrivers WHERE RowNumber > 1 
